2f2fcb8a3f816045d829e999bbf99462ffdd0729
yangm <511659192@qq.com>
2018-07-12 00:10:19 +0800

    peekNumber

--- materials2/src/main/java/com/ym/materials/gson/stream/JsonReader.java
+++ materials2/src/main/java/com/ym/materials/gson/stream/JsonReader.java
@@ -4,14 +4,19 @@ import com.ym.materials.gson.internal.Preconditions;
 import jdk.nashorn.internal.ir.IfNode;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
 
+import javax.swing.*;
 import java.io.*;
 import java.lang.annotation.Target;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.concurrent.Future;
 
 /**
  * Created by ym on 2018/7/8.
  */
 public class JsonReader extends JsonScope implements Closeable {
+
+    private static final long MIN_INCOMPLETE_INTEGER = Long.MIN_VALUE / 10;
 
     private boolean lenient;
     private int peeked = PEEKED_NONE;
@@ -20,9 +25,24 @@ public class JsonReader extends JsonScope implements Closeable {
     private static final int PEEKED_BEGIN_OBJECT = 1;
     private static final int PEEKED_END_OBJECT = 2;
 
+    private static final int PEEKED_TRUE = 5;
+    private static final int PEEKED_FALSE = 6;
     private static final int PEEKED_NULL = 7;
     private static final int PEEKED_DOUBLE_QUOTED = 9;
     private static final int PEEKED_UNQUOTED = 10;
+
+    private static final int PEEKED_LONG = 15;
+    private static final int PEEKED_NUMBER = 16;
+    private static final int PEEKED_EOF = 17;
+
+    private static final int NUMBER_CHAR_NONE = 0;
+    private static final int NUMBER_CHAR_SIGN = 1;
+    private static final int NUMBER_CHAR_DIGIT = 2;
+    private static final int NUMBER_CHAR_DECIMAL = 3;
+    private static final int NUMBER_CHAR_FRACTION_DIGIT = 4;
+    private static final int NUMBER_CHAR_EXP_E = 5;
+    private static final int NUMBER_CHAR_EXP_SIGN = 6;
+    private static final int NUMBER_CHAR_EXP_DIGIT = 7;
 
     private final Reader in;
     private final char[] buffer = new char[1024];
@@ -35,6 +55,11 @@ public class JsonReader extends JsonScope implements Closeable {
     private int stackSize = 0;
     private String[] pathNames = new String[32];
     private int[] pathIndices = new int[32];
+
+    private long peekedLong;
+    private int peekedNumberLength;
+    private String peekedString;
+
 
     {
         push(EMPTY_DOCUMENT);
@@ -80,6 +105,11 @@ public class JsonReader extends JsonScope implements Closeable {
 
         int c = nextNonWhitespace(true);
         switch (c) {
+            case '"':
+                checkLenient();
+                return peeked = PEEKED_DOUBLE_QUOTED;
+            case '{':
+                return peeked = PEEKED_BEGIN_OBJECT;
             default:
                 pos--;
         }
@@ -102,16 +132,140 @@ public class JsonReader extends JsonScope implements Closeable {
         return peeked = PEEKED_UNQUOTED;
     }
 
-    private int peekNumber() {
-        return 0;
+    private int peekNumber() throws IOException {
+        int p = pos;
+        int l = limit;
+        long value = 0;
+        boolean negative = false;
+        boolean fitsInLong = true;
+        int last = NUMBER_CHAR_NONE;
+
+        int i = 0;
+        outer:
+        for (; true; i++) {
+            if (p + i == l) {
+                if (i == buffer.length) {
+                    return PEEKED_NONE;
+                }
+                if (!fillBuffer(i + 1)) {
+                    break;
+                }
+                l = limit;
+                p = pos;
+            }
+
+            char c = buffer[pos + i];
+            switch (c) {
+                case '-':
+                    if (last == NUMBER_CHAR_NONE) {
+                        last = NUMBER_CHAR_SIGN;
+                        negative = true;
+                        continue;
+                    } else if (last == NUMBER_CHAR_EXP_E) {
+                        last = NUMBER_CHAR_EXP_SIGN;
+                        continue;
+                    }
+                    return PEEKED_NONE;
+                case '+':
+                    if (last == NUMBER_CHAR_EXP_E) {
+                        last = NUMBER_CHAR_EXP_SIGN;
+                        continue;
+                    }
+                    return PEEKED_NONE;
+                case 'e':
+                case 'E':
+                    if (last == NUMBER_CHAR_DIGIT || last == NUMBER_CHAR_FRACTION_DIGIT) {
+                        last = NUMBER_CHAR_EXP_E;
+                        continue;
+                    }
+                    return PEEKED_NONE;
+                case '.':
+                    if (last == NUMBER_CHAR_DIGIT) {
+                        last = NUMBER_CHAR_DECIMAL;
+                        continue;
+                    }
+                    return PEEKED_NONE;
+                default:
+                    if (c < '0' || c > '9') {
+                        if (!isLiteral(c)) {
+                            break outer;
+                        }
+                        return PEEKED_NONE;
+                    }
+
+                    if (last == NUMBER_CHAR_NONE || last == NUMBER_CHAR_SIGN) {
+                        value = -(c - '0');
+                        last = NUMBER_CHAR_DIGIT;
+                    } else if (last == NUMBER_CHAR_DIGIT) {
+                        if (value == 0) {
+                            return PEEKED_NONE;
+                        }
+
+                        long newValue = value * 10 - (c - '0');
+                        fitsInLong &= value > MIN_INCOMPLETE_INTEGER || (value == MIN_INCOMPLETE_INTEGER && value > newValue);
+                        value = newValue;
+                    } else if (last == NUMBER_CHAR_DECIMAL) {
+                        last = NUMBER_CHAR_FRACTION_DIGIT;
+                    } else if (last == NUMBER_CHAR_EXP_E || last == NUMBER_CHAR_EXP_SIGN) {
+                        last = NUMBER_CHAR_EXP_DIGIT;
+                    }
+            }
+        }
+
+        if (last == NUMBER_CHAR_DIGIT && fitsInLong && (value != Long.MIN_VALUE || negative) && (value != 0 || !negative)) {
+            peekedLong = negative ? value : -value;
+            pos += i;
+            return peeked = PEEKED_LONG;
+        } else if (last == NUMBER_CHAR_DIGIT || last == NUMBER_CHAR_FRACTION_DIGIT || last == NUMBER_CHAR_EXP_DIGIT) {
+            peekedNumberLength = i;
+            return peeked = PEEKED_NUMBER;
+        } else {
+            return PEEKED_NONE;
+        }
     }
 
-    private int peekKeyWork() {
-        return 0;
+    private int peekKeyWork() throws IOException {
+        char c = buffer[pos];
+        String keyword;
+        String keyworkUpper;
+        int peeking;
+        if (c == 't' || c == 'T') {
+            keyword = "true";
+            keyworkUpper = "TRUE";
+            peeking = PEEKED_TRUE;
+        } else if (c == 'f' || c == 'F') {
+            keyword = "false";
+            keyworkUpper = "FALSE";
+            peeking = PEEKED_FALSE;
+        } else if (c == 'n' || c == 'N') {
+            keyword = "null";
+            keyworkUpper = "NULL";
+            peeking = PEEKED_NULL;
+        } else {
+            return PEEKED_NONE;
+        }
+
+        int length = keyword.length();
+        for (int i = 1; i < length; i++) {
+            if (pos + i >= limit && !fillBuffer(i + 1)) {
+                return PEEKED_NONE;
+            }
+            c = buffer[pos + i];
+            if (c != keyword.charAt(i) && c != keyworkUpper.charAt(i)) {
+                return PEEKED_NONE;
+            }
+        }
+
+        if ((pos + length < limit || fillBuffer(length + 1)) && isLiteral(buffer[pos + length])) {
+            return PEEKED_NONE;
+        }
+
+        pos += length;
+        return peeked = peeking;
     }
 
     private int nextNonWhitespace(boolean throwOnEof) throws IOException {
-        for (;;) {
+        for (; ; ) {
             if (pos == limit) {
                 boolean filled = fillBuffer(1);
                 if (!filled) {
@@ -172,7 +326,7 @@ public class JsonReader extends JsonScope implements Closeable {
     }
 
     private void skipToEndLine() throws IOException {
-        for (;pos < limit || fillBuffer(1); pos++) {
+        for (; pos < limit || fillBuffer(1); pos++) {
             char c = buffer[pos];
             if (c == '\n') {
                 lineNumber++;
@@ -187,7 +341,7 @@ public class JsonReader extends JsonScope implements Closeable {
     private boolean skipTo(String toFind) throws IOException {
         int length = toFind.length();
         outer:
-        for (;pos <= limit || fillBuffer(length); pos++) {
+        for (; pos <= limit || fillBuffer(length); pos++) {
             char c = buffer[pos];
             if (c == '\n') {
                 lineNumber++;
@@ -215,7 +369,7 @@ public class JsonReader extends JsonScope implements Closeable {
         }
 
         pos = 0;
-        for (int total; (total = in.read(buffer, limit, buffer.length - limit)) != 0;) {
+        for (int total; (total = in.read(buffer, limit, buffer.length - limit)) != 0; ) {
             limit += total;
             if (lineNumber == 0 && lineStart == 0 && limit > 0 && buffer[0] == '\ufeff') {
                 lineStart++;
--- openSource/src/main/java/com/ym/materials/gson/GsonDemo2.java
+++ openSource/src/main/java/com/ym/materials/gson/GsonDemo2.java
@@ -6,6 +6,7 @@ import sun.misc.Unsafe;
 
 import java.io.StringReader;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,15 @@ public class GsonDemo2 {
         private String name;
         private Person child;
         private List<Integer> ids;
+        private BigDecimal num;
+
+        public BigDecimal getNum() {
+            return num;
+        }
+
+        public void setNum(BigDecimal num) {
+            this.num = num;
+        }
 
         public String getName() {
             return name;
@@ -43,12 +53,18 @@ public class GsonDemo2 {
     public static void main(String[] args) {
         Gson gson = new Gson();
         Person person = new Person();
-        person.setName("name");
+        person.setName("01");
+        System.out.println(Long.toBinaryString(Long.MIN_VALUE));
+        System.out.println(Long.toBinaryString(Long.MIN_VALUE + 1));
+        person.setNum(new BigDecimal(-Long.MIN_VALUE));
+        System.out.println(Integer.toBinaryString(15));
+        System.out.println(Integer.toBinaryString(12));
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
-        person.setIds(list);
+//        person.setIds(list);
         String aa = gson.toJson(person);
         System.out.println(aa);
+        gson.fromJson(aa, Person.class);
     }
 }
